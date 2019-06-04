afc608127ba64f151205def17604a22f4cd7adae
livalex <livalex11@gmail.com>
2019-06-04 16:49:03 +0300

    java config with qualifier

--- src/main/java/org/tyaa/springcontextdemo/configs/AppConfig.java
+++ src/main/java/org/tyaa/springcontextdemo/configs/AppConfig.java
@@ -1,5 +1,7 @@
 package org.tyaa.springcontextdemo.configs;
 
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.tyaa.springcontextdemo.entity.Unit;
@@ -13,15 +15,23 @@ public class AppConfig {
 		return new Unit();
 	}
 	
-	@Bean("weapon-sword")
+	@Bean(name = "weapon-sword")
 	public Object getSword() {
 		Weapon weapon = new Weapon();
 		weapon.name = "sword";
 		return weapon;
 	}
+        
+        @Bean("weapon-axe")
+        @Qualifier(value = "weapon-axe")
+	public Object getWeapon() {
+		Weapon weapon = new Weapon();
+		weapon.name = "axe";
+		return weapon;
+	}
 	
 	@Bean("warrior")
-	public Object getWarrior(Weapon _weapon) {
+	public Object getWarrior(@Qualifier(value = "weapon-axe") Weapon _weapon) {
 		Unit unit = new Unit();
 		unit.name = _weapon.getName() + "sman";
 		return unit;
